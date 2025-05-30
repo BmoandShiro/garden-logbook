ff8427ac3e2f3a40843e87967936245c1dcdde32
BMO <bmoandshiro@protonmail.com>
2025-05-30 01:22:51 -0400

    fixed cron. testes in 1 min intervals switched back to 4 hour intervals but need to check

--- app/logs/[id]/LogDateField.tsx
+++ app/logs/[id]/LogDateField.tsx
@@ -1,12 +1,12 @@
 'use client';
-import { format, toZonedTime } from 'date-fns-tz';
+import { format, toZonedTime, formatInTimeZone } from 'date-fns-tz';
 
 export default function LogDateField({ date, timezone }: { date: string, timezone?: string }) {
   let utcString = 'Invalid date';
   let gardenString = null;
   const d = new Date(date);
   if (date && !isNaN(d.getTime())) {
-    utcString = format(d, 'yyyy-MM-dd HH:mm:ss') + ' UTC';
+    utcString = formatInTimeZone(d, 'UTC', 'yyyy-MM-dd HH:mm:ss') + ' UTC';
     if (timezone) {
       const zonedDate = toZonedTime(d, timezone);
       gardenString = format(zonedDate, 'yyyy-MM-dd hh:mm a zzz', { timeZone: timezone });
